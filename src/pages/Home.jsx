9eee7dda03502507971ccfcadd299c4669aec0d9
Ryan David Riberio Dutra <cru0168995@aluno.firjansenaisesi.com.br>
2026-03-04 16:34:44 -0300

    commit do final valeu

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+
+    return (
+      <>
+        <h1>Bem-vindo à Home!</h1>
+  
+      </>
+    )
+  }
+  
+  export default Home
+  
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+function Login() {
+
+    return (
+      <>
+        <h1>Login</h1>
+        <link to={"/>cadastro"}></link>
+  
+      </>
+    )
+  }
+  
+  export default Login
+  
